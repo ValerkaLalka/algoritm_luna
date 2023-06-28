03f05407b92658205dd30480a7b5ce90dea1c013
ValerkaLalka <valerysimanovich@gmail.com>
2023-06-28 23:30:54 +0300

    I add convert string to int

--- algoritm_luna.cpp
+++ algoritm_luna.cpp
@@ -1,68 +1,43 @@
-#include <stdio.h>
-enum CardType
-{
-  type_master_card,
-  type_visa,
-  type_american_express,
-  type_other
-};
- 
-static char* EmitentNames[] = {"Master Card", "VISA", "American Express", "Other"};
- 
-int GetFirstTwoDigits(const int* const cardNumber)
-{
-  return (cardNumber[0] * 10 + cardNumber[1]);
-}
- 
-enum CardType GetCardType(const int* const cardNumber)
-{
-  enum CardType type;
- 
-  int firstTwoDigits = GetFirstTwoDigits(cardNumber);
- 
-  if ((firstTwoDigits >= 51) && (firstTwoDigits <= 55))       type = type_master_card;
-  else if (firstTwoDigits / 10 == 4)                          type = type_visa;
-  else if ((firstTwoDigits == 34) || (firstTwoDigits == 37))  type = type_american_express;
-  else                                                        type = type_other;
-  return type;
-}
- 
-int Luhn(const int* const cardNumber, const int cardNumberLength)
-{
-  int validationFlag = 0;
-  int p = 0;
-  int sum = 0;
-  int i = 0;
- 
-  for (i = cardNumberLength - 1; i > -1; i--)
-  {
-    p = cardNumber[i];
- 
-    if ((i + 1) % 2 == 0) p = p * 2;
- 
-    if (p > 9) p = p - 9;
- 
-    sum = sum + p;
-  }
- 
-  if (sum % 10 == 0) validationFlag = 1;
- 
-  return validationFlag;
-}
- 
-void VerifyCard(const int* const cardNumber, const int cardNumberLength)
-{
-  if (Luhn(cardNumber, cardNumberLength))
-    printf("This is a VALID credit card. Emitent: %s\n", EmitentNames[GetCardType(cardNumber)]);
-  else
-    printf("This card is INVALID!\n");
-}
- 
-int main(void)
-{
-  int card[] = {3, 7, 8, 7, 3, 4, 4, 9, 3, 6, 7, 1, 0, 0, 0};
- 
-  VerifyCard(card, sizeof(card)/ sizeof(*card));
- 
-  return 0;
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+   string word;
+   string new_word = "";
+   cout << "Write your number: ";
+   getline(cin, word); //присваиваем word значение, которое мы вводим
+   
+   for (int i = 0; i < word.size(); i++) { //удаление пробелов из word
+        if (!isspace(word[i])) {
+            new_word += word[i];
+        }
+    }
+   
+    char num[new_word.length()]; //char arr[word.length()]; // создаем массив символов длиной равной длине введенного слова
+   
+   for (int i = 0; i < new_word.length(); i++) {
+		num[i] = new_word[i]; // заполняем массив символами из введенного слова
+   }
+
+   
+   //for (int i = 0; i < new_word.length(); i++){
+	   //int z = i;
+	   //z++;
+	   //if (z % 2 == 0){
+		   //new_word[i] = new_word[i] * 2;
+	   //}
+   //}
+   
+   
+   cout << "Array of words: ";
+   for (int i = 0; i < new_word.length(); i++) {
+      cout << num[i] << "|"; // выводим массив символов на экран
+   }
+   
+   cout << endl << new_word.length() << endl << new_word;
+   
+   return 0;
 }
