244eaa3659a20d014f04fe2f276b9eb4a38a9525
ValerkaLalka <valerysimanovich@gmail.com>
2023-07-24 18:34:01 +0300

    Алгоритм луна готов

--- algoritm_luna.cpp
+++ algoritm_luna.cpp
@@ -5,39 +5,72 @@
 using namespace std;
 
 int main() {
-   string word;
-   string new_word = "";
-   cout << "Write your number: ";
-   getline(cin, word); //присваиваем word значение, которое мы вводим
+	int chet[8];
+	int nechet[8];
+	int a = 0;
+	int sum = 0;
+	int p = 0;
+	int l = 0;
+	string word;
+	string new_word = "";
+	cout << "Write your number: ";
+	getline(cin, word); //присваиваем word значение, которое мы вводим
    
-   for (int i = 0; i < word.size(); i++) { //удаление пробелов из word
-        if (!isspace(word[i])) {
-            new_word += word[i];
+	for (int i = 0; i < word.size(); i++) { //удаление пробелов из word
+		if (!isspace(word[i])) {
+			new_word += word[i];
         }
     }
    
-    char num[new_word.length()]; //char arr[word.length()]; // создаем массив символов длиной равной длине введенного слова
-   
-   for (int i = 0; i < new_word.length(); i++) {
-		num[i] = new_word[i]; // заполняем массив символами из введенного слова
-   }
+	int num[new_word.length()]; // создаем массив символов длиной равной длине введенного слова
 
-   
-   //for (int i = 0; i < new_word.length(); i++){
-	   //int z = i;
-	   //z++;
-	   //if (z % 2 == 0){
-		   //new_word[i] = new_word[i] * 2;
-	   //}
-   //}
-   
-   
-   cout << "Array of words: ";
-   for (int i = 0; i < new_word.length(); i++) {
-      cout << num[i] << "|"; // выводим массив символов на экран
-   }
-   
-   cout << endl << new_word.length() << endl << new_word;
-   
-   return 0;
+	for (int i = 0; i < new_word.length(); i++) {
+		num[i] = stoi(string(1, new_word[i]));
+	}
+	
+	if (new_word.length() == 16){
+		for (int i = 0; i < new_word.length(); i++){
+			if ((i + 1) % 2 == 0){
+				chet[p] = num[i];
+				p++;
+			}
+		}
+		
+		p = 0;
+		
+		for (int i = 0; i < new_word.length(); i++) {
+			if ((i + 1) % 2 != 0){ //умножение каждого числа, которое четное по счету на 2
+				nechet[p] = num[i] * 2;
+				if (nechet[p] > 9){
+					a = nechet[p] - 10;
+					a += 1;
+					nechet[p] = a;
+				}
+				p++;
+			}
+			/*else{
+				chet[p] = num[i];
+				p++;
+			}*/
+		}
+	}
+	else{
+		cout << "You wrote wrong number of your card!";
+	}
+	
+	cout << "Sum: ";
+	for (p = 0; p < 8; p++){
+		sum += nechet[p] + chet[p];
+	}
+	
+	cout << sum;
+	
+	if (sum % 10 == 0){
+		cout << endl << "Your card is valid";
+	}
+	else{
+		cout << endl << "Your card is invalid";
+	}
+	
+	return 0;
 }
